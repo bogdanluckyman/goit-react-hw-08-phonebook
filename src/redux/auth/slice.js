698645340bfb4648efc338f878b83ed1979607b1
Bogdan <bogdantisenko06@gmail.com>
2023-12-26 12:06:55 +0200

    refresh bag

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -11,35 +11,29 @@ const authSlice = createSlice({
   },
   extraReducers: builder => {
     builder
-      .addCase(register.pending, (state, action) => state)
+
       .addCase(register.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
       })
-      .addCase(register.rejected, (state, action) => state)
-      .addCase(login.pending, (state, action) => state)
+
       .addCase(login.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
       })
-      .addCase(login.rejected, (state, action) => state)
-      .addCase(logout.pending, (state, action) => state)
 
       .addCase(logout.fulfilled, state => {
         state.user = { name: null, email: null };
         state.token = null;
         state.isLoggedIn = false;
       })
-      .addCase(logout.rejected, (state, action) => state)
-      .addCase(refresh.pending, (state, action) => state)
 
       .addCase(refresh.fulfilled, (state, action) => {
         state.user = action.payload;
         state.isLoggedIn = true;
-      })
-      .addCase(refresh.rejected, (state, action) => state);
+      });
   },
 });
 
